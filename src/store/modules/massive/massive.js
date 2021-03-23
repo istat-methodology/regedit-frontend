79ae86de86e1b751de4aa762dbad33f5d16bac85
SMacOne <s.macone@libero.it>
2021-03-23 12:07:01 +0100

    fix

--- src/store/modules/massive/massive.js
+++ src/store/modules/massive/massive.js
@@ -4,8 +4,10 @@ const actions = {
   update({ dispatch }, payload) {
     return massiveService
       .update(payload)
-      .then(dispatch("message/success", "Data updated!"), {
-        root: true
+      .then(() => {
+        dispatch("message/success", "Dati aggiornati!", {
+          root: true
+        });
       })
       .catch(err => {
         console.log(err);
