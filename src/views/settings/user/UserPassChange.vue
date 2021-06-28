060f406e52951885238c3f833519628fe501e72f
SMacOne <s.macone@libero.it>
2021-06-28 11:39:24 +0200

    change password form

--- src/views/settings/user/UserPassChange.vue
+++ src/views/settings/user/UserPassChange.vue
@@ -17,106 +17,105 @@
         </header>
 
         <div class="card-body" v-if="user">
-          <div
-            class="input-group mb-3"
-            :class="{ 'form-group--error': $v.user.name.$error }"
-          >
+          <div class="input-group mb-3">
             <div class="input-group-prepend">
               <span class="input-group-text" id="inputGroup-sizing-default"
                 >Name</span
               >
             </div>
             <input
-              class="form-control"
               aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-default"
+              readonly
               v-model="user.name"
             />
-            <div class="row col-12">
-              <div class="error" v-if="!$v.user.name.required">
-                Name is required
-              </div>
-              <div class="error" v-if="!$v.user.name.minLength">
-                Name must have at least
-                {{ $v.user.name.$params.minLength.min }} letters.
-              </div>
-            </div>
           </div>
 
-          <div
-            class="input-group mb-3"
-            :class="{ 'form-group--error': $v.user.surname.$error }"
-          >
+          <div class="input-group mb-3">
             <div class="input-group-prepend">
               <span class="input-group-text" id="inputGroup-sizing-default"
                 >Surname</span
               >
             </div>
             <input
-              class="form-control"
               aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-default"
+              readonly
               v-model="user.surname"
             />
-            <div class="row col-12">
-              <div class="error" v-if="!$v.user.surname.required">
-                Surname is required
-              </div>
-              <div class="error" v-if="!$v.user.surname.minLength">
-                Name must have at least
-                {{ $v.user.surname.$params.minLength.min }} letters.
-              </div>
-            </div>
           </div>
 
           <div
             class="input-group mb-3"
-            :class="{ 'form-group--error': $v.user.email.$error }"
+            :class="{ 'form-group--error': $v.oldPassword.$error }"
           >
             <div class="input-group-prepend">
               <span class="input-group-text" id="inputGroup-sizing-default"
-                >Email</span
+                >Vecchia password</span
               >
             </div>
             <input
               class="form-control"
               aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-default"
-              v-model="user.email"
+              v-model="oldPassword"
             />
             <div class="row col-12">
-              <div class="error" v-if="!$v.user.email.required">
-                Email is required
+              <div class="error" v-if="!$v.oldPassword.required">
+                password is required
               </div>
-              <div class="error" v-if="!$v.user.email.email">
-                must have a valid
-                {{ $v.user.email.$params.email }}.
+              <div class="error" v-if="!$v.oldPassword.minLength">
+                password must have at least
+                {{ $v.oldPassword.$params.minLength.min }} letters.
               </div>
             </div>
           </div>
           <div
             class="input-group mb-3"
-            :class="{ 'form-group--error': $v.user.role.$error }"
+            :class="{ 'form-group--error': $v.newPassword.$error }"
           >
             <div class="input-group-prepend">
               <span class="input-group-text" id="inputGroup-sizing-default"
-                >Role</span
+                >Nuova password</span
               >
             </div>
-
-            <v-select
-              label="role"
-              :options="roles"
-              placeholder="role"
-              v-model="user.role"
-            ></v-select>
-
+            <input
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              v-model="newPassword"
+            />
             <div class="row col-12">
-              <div class="error" v-if="!$v.user.role.required">
-                role is required
+              <div class="error" v-if="!$v.newPassword.required">
+                password is required
+              </div>
+              <div class="error" v-if="!$v.newPassword.minLength">
+                password must have at least
+                {{ $v.newPassword.$params.minLength.min }} letters.
               </div>
             </div>
           </div>
+          <!--  <div
+            class="input-group mb-3"
+            :class="{ 'form-group--error': $v.newRepeatedPassword.$error }"
+          >
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-default"
+                >Ripeti password</span
+              >
+            </div>
+            <input
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              v-model="newRepeatedPassword"
+            />
+            <div class="row col-12">
+              <div class="error" v-if="!$v.newRepeatedPassword.sameAsPassword">
+                La password deve essere identica.
+              </div>
+            </div>
+          </div> -->
 
           <div class="card-footer">
             <button
@@ -127,11 +126,11 @@
               Update!
             </button>
 
-            <Button
+            <!--   <Button
               class="btn btn-outline-dark btn-sm"
               @click.prevent="handleReset()"
               >Reset</Button
-            >
+            > -->
           </div>
           <div class="row col-12">
             <p class="typo__p" v-if="submitStatus === 'OK'">
@@ -152,33 +151,29 @@
 
 <script>
 import { mapGetters } from "vuex";
-import { required, minLength, email } from "vuelidate/lib/validators";
+import { required, minLength /* sameAs */ } from "vuelidate/lib/validators";
 export default {
-  name: "UserEdit",
+  name: "UserPassChange",
   data() {
     return {
-      submitStatus: null
-      //disabled: true
+      submitStatus: null,
+      oldPassword: "",
+      newPassword: "",
+      newRepeatedPassword: ""
     };
   },
   validations: {
-    user: {
-      name: {
-        required,
-        minLength: minLength(4)
-      },
-      surname: {
-        required,
-        minLength: minLength(4)
-      },
-      email: {
-        required,
-        email
-      },
-      role: {
-        required
-      }
+    oldPassword: {
+      required,
+      minLength: minLength(8)
+    },
+    newPassword: {
+      required,
+      minLength: minLength(8)
     }
+    /* newRepeatedPassword: {
+      sameAsPassword: sameAs("newPassword")
+    } */
   },
   computed: {
     ...mapGetters("user", ["user"]),
@@ -186,7 +181,7 @@ export default {
   },
   created() {
     this.$store.dispatch("user/findById", this.$route.params.id);
-    this.$store.dispatch("role/findAll");
+    //this.$store.dispatch("role/findAll");
   },
   methods: {
     /*  changeRole(value) {
@@ -200,20 +195,19 @@ export default {
           id: this.user.id,
           name: this.user.name,
           surname: this.user.surname,
-          email: this.user.email,
-          roleid: this.user.role.id
+          oldPassword: this.oldPassword,
+          newPassword: this.newPassword
         };
         this.$store.dispatch("user/update", data);
       }
-    },
-    handleReset() {
+    }
+    /* handleReset() {
       this.user.name = "";
       this.user.surname = "";
-      this.user.email = "";
-      /*  this.user.password = ""; */
-      this.user.role = null;
+      this.oldPassword = null;
+      this.newPassword = null;
       this.$v.$reset();
-    }
+    } */
   }
 };
 </script>
