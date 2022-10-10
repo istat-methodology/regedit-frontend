5d958f3c0cb5c197669cf19dacc673e8cc5f0839
SMacOne <s.macone@libero.it>
2022-10-10 11:35:28 +0200

    fix

--- src/common/address.util.js
+++ src/common/address.util.js
@@ -2,9 +2,20 @@ import store from "@/store";
 
 export function getUserId() {
   let id = -1;
-  if (store.getters["auth/isSupervisor"]) {
+  if (
+    store.getters["auth/isSupervisor"] &&
+    store.getters["customswitch/toponimi"]
+  ) {
     id = store.getters["address/assignedId"];
-    if (id == -1) id = store.getters["toponimo/assignedTopId"];
+  } else {
+    let user = store.getters["auth/loggedUser"];
+    if (user) id = user.userId;
+  }
+  if (
+    store.getters["auth/isSupervisor"] &&
+    !store.getters["customswitch/toponimi"]
+  ) {
+    id = store.getters["toponimo/assignedTopId"];
   } else {
     let user = store.getters["auth/loggedUser"];
     if (user) id = user.userId;
