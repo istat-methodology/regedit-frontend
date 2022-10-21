1d14a7954b4c446f7d80e81716c43f18000f4067
SMacOne <s.macone@libero.it>
2022-10-21 18:55:43 +0200

    fix

--- src/views/catalogue/toponimo/domain/ToponimoRevisedEdit.vue
+++ src/views/catalogue/toponimo/domain/ToponimoRevisedEdit.vue
@@ -32,43 +32,6 @@
           'is-invalid': $v.toponimo.dufVal.$error
         }"
       />
-      <!-- <CInput
-        label="Civico"
-        placeholder="Civico"
-        v-model="toponimo.civicoVal"
-        :class="{
-          'is-invalid': $v.toponimo.civicoVal.$error
-        }"
-      />
-      <p class="error" v-if="!$v.toponimo.civicoVal.validationRuleCivico">
-        I valori possibili per questo campo sono soltanto numerici
-      </p>
-      <CInput label="Km" placeholder="Km" v-model="toponimo.kmVal" />
-      <CInput
-        label="Esponente"
-        placeholder="Esponente"
-        v-model="toponimo.esponenteVal"
-        :class="{
-          'is-invalid': $v.toponimo.esponenteVal.$error
-        }"
-      />
-      <p class="error" v-if="!$v.toponimo.esponenteVal.validationRule">
-        I valori possibili per questo campo sono: · Non valorizzato. . Numerico
-        Un solo carattere alfabetico.
-        ·'BIS','TER','QUATER','QUINQUIES','SEXIES','SEPTIES’ · Tutte le
-        casistiche precedenti seguite da ' R' o ' ROSSO'
-      </p>
-      <label>Fonte*</label>
-      <v-select
-        label="fonte"
-        :options="fonteValues"
-        v-model="fonteLocal"
-        placeholder="Fonte"
-        :class="{
-          'is-invalid': $v.fonteLocal.$error
-        }"
-      ></v-select> -->
-      <!-- <template v-if="isFonteEgon"> -->
       <template>
         <CInput
           label="Codice strada*"
@@ -82,29 +45,7 @@
           I valori possibili per questo campo sono soltanto numerici e lunghi
           massimo 12 caratteri
         </p>
-        <!-- <CInput
-          label="Codice civico"
-          placeholder="Codice civico"
-          v-model="toponimo.cdpcivEgon"
-          :class="{
-            'is-invalid': $v.toponimo.cdpcivEgon.$error
-          }"
-        />
-        <p class="error" v-if="$v.toponimo.cdpcivEgon.$error">
-          I valori possibili per questo campo sono soltanto numerici e lunghi
-          massimo 15 caratteri
-        </p> -->
       </template>
-      <!--  <label>Fittizio*</label>
-      <v-select
-        label="value"
-        :options="fittizioValues"
-        v-model="fittizioLocal"
-        placeholder="Fittizio"
-        :class="{
-          'is-invalid': $v.fittizioLocal.$error
-        }"
-      ></v-select> -->
     </CCardBody>
     <CCardFooter>
       <CButton shape="square" size="sm" color="danger" @click="handleSubmit"
@@ -116,28 +57,10 @@
 <script>
 import { required, maxLength } from "vuelidate/lib/validators";
 import { mapGetters } from "vuex";
-import fonteMixin from "@/components/mixins/fonte.mixin";
-import fittizioMixin from "@/components/mixins/fittizio.mixin";
-
 export default {
   name: "ToponimoRevisedEdit",
-  mixins: [fonteMixin, fittizioMixin],
-  data: function() {
-    return {
-      fonteLocal: this.fonte,
-      fittizioLocal: this.fittizio
-    };
-  },
   props: {
     toponimo: {
-      type: Object,
-      default: () => null
-    },
-    fonte: {
-      type: Object,
-      default: () => null
-    },
-    fittizio: {
       type: Object,
       default: () => null
     }
@@ -149,17 +72,8 @@ export default {
         return dug.name;
       });
     }
-    /* isFonteEgon() {
-      return this.fonteLocal && this.fonteLocal.id == 1 ? true : false;
-    } */
   },
   validations: {
-    /* fonteLocal: {
-      required
-    },
-    fittizioLocal: {
-      required
-    }, */
     toponimo: {
       dugVal: {
         required
@@ -168,38 +82,14 @@ export default {
         required
       },
       cdpstrVal: {
-        validationRuleStr(cdpstrVal) {
-          return this.cdpstrVal ? /^[0-9?]+$/.test(cdpstrVal) : true;
+        validationRuleStr() {
+          return (
+            /^[0-9?]*$/.test(this.toponimo.cdpstrVal) ||
+            /^[null]/.test(this.toponimo.cdpstrVal)
+          );
         },
         maxLength: maxLength(12)
       }
-
-      /*  civicoVal: {
-        validationRuleCivico(civico) {
-          return /^[0-9?]*$/.test(civico) || /^[null]/.test(civico);
-        }
-      },
-      esponenteVal: {
-        validationRule(esponente) {
-          return (
-            /^[a-zA-Z?]$/.test(esponente) ||
-            /^[null]/.test(esponente) ||
-            /^[0-9?]*$/.test(esponente) ||
-            /(BIS|TER|QUATER|QUINQUIES|SEXIES|SEPTIES)\b/.test(esponente) ||
-            /^[a-zA-Z?](R|ROSSO)$/.test(esponente) ||
-            /^[0-9?]*(R|ROSSO)$/.test(esponente) ||
-            /(BIS|TER|QUATER|QUINQUIES|SEXIES|SEPTIES)(R|ROSSO)\b/.test(
-              esponente
-            )
-          );
-        }
-      }, */
-      /* cdpcivEgon: {
-        validationRuleCivEgon(civicoEgon) {
-          return /^[0-9?]*$/.test(civicoEgon) || /^[null]/.test(civicoEgon);
-        },
-        maxLength: maxLength(15)
-      } */
     }
   },
   methods: {
@@ -218,8 +108,8 @@ export default {
         } */
         this.$emit("revise", this.toponimo);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
