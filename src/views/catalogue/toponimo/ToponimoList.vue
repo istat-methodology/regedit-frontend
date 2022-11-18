59e9a77a4a3f031e435ea48b843906777e32ca58
SMacOne <s.macone@libero.it>
2022-11-18 15:05:32 +0100

    fix

--- src/views/catalogue/toponimo/ToponimoList.vue
+++ src/views/catalogue/toponimo/ToponimoList.vue
@@ -85,12 +85,10 @@ export default {
         toponimoList: toponimoList
       };
       this.$store.dispatch("massive/update", payload).then(() => {
-        this.$store.dispatch("toponimo/setFilterTopSoglia", null).then(() => {
-          this.$store.dispatch(
-            "toponimo/findByUserAndState",
-            this.$route.params.state
-          );
-        });
+        this.$store.dispatch(
+          "toponimo/findByUserAndState",
+          this.$route.params.state
+        );
       });
       console.log(toponimoList.toString + "-" + dug + "-" + duf + "-" + note);
     },
@@ -135,10 +133,12 @@ export default {
         toponimoList: toponimoList
       };
       this.$store.dispatch("massiveTopVal/update", payload).then(() => {
-        this.$store.dispatch(
-          "toponimo/findByUserAndState",
-          this.$route.params.state
-        );
+        this.$store.dispatch("toponimo/setFilterTopSoglia", null).then(() => {
+          this.$store.dispatch(
+            "toponimo/findByUserAndState",
+            this.$route.params.state
+          );
+        });
         this.$store.dispatch("progressTop/findByUser");
       });
     },
